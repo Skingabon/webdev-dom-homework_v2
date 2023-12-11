6f4dee84ceeecb6fec58a752ad2f7cd85518e4bc
Your Name <you@example.com>
2023-12-11 12:46:34 +0300

    Остались незакомиченные после коммита. почему - не понятно.

--- autorize.js
+++ autorize.js
@@ -5,19 +5,19 @@ export let userLogin = localStorage.getItem('login');
 export let token = localStorage.getItem('token');
 export const setToken = (newToken) => {
     token = newToken;
-}
+};
 
 export function autorizeRender(isLoginMode = true) {
     const appHTML = document.getElementById('app');
-    const loginHTML =
-
-        `<div class = 'autorizeForm'> 
-        <h1 class = 'form-title'>Форма ${isLoginMode ? "входа" : "Регистрации"}</h1>
+    const loginHTML = `<div class = 'autorizeForm'> 
+        <h1 class = 'form-title'>Форма ${
+            isLoginMode ? 'входа' : 'Регистрации'
+        }</h1>
         <div>
-        ${isLoginMode
-            ?
-            ""
-            : `
+        ${
+            isLoginMode
+                ? ''
+                : `
             <input type="text" 
 class="add-form-name" 
 placeholder="Введите ваше имя" 
@@ -33,79 +33,86 @@ id="input-name2" />
         placeholder="Введите ваш пароль" 
         id="input-password"></textarea>
         <div class="add-form-row">
-        <button class="add-form-button" id="login-button">${isLoginMode ? "Войти" : "Зарегистрироваться"}</button>
-        <button class="add-form-button" id="toggle-button">Перейти ${isLoginMode ? "К регистрации" : "ко входу"} </button>
+        <button class="add-form-button" id="login-button">${
+            isLoginMode ? 'Войти' : 'Зарегистрироваться'
+        }</button>
+        <button class="add-form-button" id="toggle-button">Перейти ${
+            isLoginMode ? 'К регистрации' : 'ко входу'
+        } </button>
 </div>
 </div>
-        `
+        `;
     appHTML.innerHTML = loginHTML;
 
-    document.getElementById("toggle-button").addEventListener('click', () => {
+    document.getElementById('toggle-button').addEventListener('click', () => {
         isLoginMode = !isLoginMode;
-        console.log("Я в кнопке перейти к регистрации");
+        console.log('Я в кнопке перейти к регистрации');
         autorizeRender(isLoginMode);
-    })
+    });
 
-    document.getElementById("login-button").addEventListener('click', () => {
+    document.getElementById('login-button').addEventListener('click', () => {
         if (isLoginMode) {
-            const loginForm = document.getElementById("input-login").value;
-            const passwordForm = document.getElementById("input-password").value;
+            const loginForm = document.getElementById('input-login').value;
+            const passwordForm =
+                document.getElementById('input-password').value;
             if (!loginForm) {
-                alert("Введите Ваш логин")
+                alert('Введите Ваш логин');
                 return;
             }
             if (!passwordForm) {
-                alert("Введите Ваш пароль")
+                alert('Введите Ваш пароль');
                 return;
             }
             login({
                 login: loginForm,
                 password: passwordForm,
-            }).then((response) => {
-                token = response.user.token;
-                userName = response.user.name;
-                console.log(userName);
-                localStorage.setItem('user', userName);
-                localStorage.setItem('token', token);
-                renderComment();
-            }).catch((error) => {
-                alert(error.message)
             })
+                .then((response) => {
+                    token = response.user.token;
+                    userName = response.user.name;
+                    console.log(userName);
+                    localStorage.setItem('user', userName);
+                    localStorage.setItem('token', token);
+                    renderComment();
+                })
+                .catch((error) => {
+                    alert(error.message);
+                });
         } else {
-
-            const nameForm = document.getElementById("input-name2").value;
-            const loginForm = document.getElementById("input-login").value;
-            const passwordForm = document.getElementById("input-password").value;
+            const nameForm = document.getElementById('input-name2').value;
+            const loginForm = document.getElementById('input-login').value;
+            const passwordForm =
+                document.getElementById('input-password').value;
 
             if (!nameForm) {
-                alert("Введите Ваше имя")
+                alert('Введите Ваше имя');
                 return;
             }
             if (!loginForm) {
-                alert("Введите Ваш логин")
+                alert('Введите Ваш логин');
                 return;
             }
             if (!passwordForm) {
-                alert("Введите Ваш пароль")
+                alert('Введите Ваш пароль');
                 return;
             }
             register({
                 login: loginForm,
                 password: passwordForm,
                 name: nameForm,
-            }).then((user) => {
-                setToken(`Bearer ${user.user.token}`)
-                token = user.user.token;
-                userName = user.user.name;
-                localStorage.setItem('user', userName);
-                localStorage.setItem('token', token);
-                renderComment();
-            }).catch((error) => {
-                alert(error.message)
             })
+                .then((user) => {
+                    setToken(`Bearer ${user.user.token}`);
+                    token = user.user.token;
+                    userName = user.user.name;
+                    localStorage.setItem('user', userName);
+                    localStorage.setItem('token', token);
+                    renderComment();
+                })
+                .catch((error) => {
+                    alert(error.message);
+                });
         }
-    }
-    )
+    });
 }
 autorizeRender();
-
--- scr/index1.js
+++ scr/index1.js
@@ -1,7 +1,7 @@
 const add = (a, b) => {
     if (a === b) {
-        return a + b + ""
+        return a + b + '';
     }
-    return a - b - ""
-}
-add(1, 2)
+    return a - b - '';
+};
+add(1, 2);
